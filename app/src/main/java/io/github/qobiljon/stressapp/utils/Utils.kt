fe73f1da2ce4a0dcee598f3561dd276807aefd9e
qobiljon <qobiljon.toshnazarov@gmail.com>
2022-11-25 15:08:48 +0900

    PSS-4 questions taken from microEMA

--- app/src/main/java/io/github/qobiljon/stressapp/utils/Utils.kt
+++ app/src/main/java/io/github/qobiljon/stressapp/utils/Utils.kt
@@ -1,10 +1,31 @@
 package io.github.qobiljon.stressapp.utils
 
 import android.content.Context
+import android.net.Uri
 import android.widget.Toast
 
 object Utils {
     fun toast(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    }
+
+    fun getCalendarEvents(context: Context) {
+        context.contentResolver.query(
+            Uri.parse("content://com.android.calendar/events"),
+            arrayOf("calendar_id", "title", "description", "dtstart", "dtend", "eventLocation"),
+            null, null, null
+        )?.let { cursor ->
+            val names = mutableListOf<String>()
+            val descriptions = mutableListOf<String>()
+            val starts = mutableListOf<String>()
+            val ends = mutableListOf<String>()
+
+            if (cursor.moveToFirst())
+                do {
+                    names.add(cursor.getString(cursor.getColumnIndex("")))
+                } while (cursor.moveToNext())
+
+            cursor.close()
+        }
     }
 }
