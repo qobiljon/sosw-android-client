dd00fc3862c3118fd12478808e229c44cdc149fd
Behzod Halil <behzoddev@gmail.com>
2022-12-27 15:30:45 +0900

    refactor::add hilt annotation to application class

--- app/src/main/java/io/github/qobiljon/stressapp/AndroidApplication.kt
+++ app/src/main/java/io/github/qobiljon/stressapp/AndroidApplication.kt
@@ -4,9 +4,11 @@ import android.app.Application
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
+import dagger.hilt.android.HiltAndroidApp
 import io.github.qobiljon.stressapp.core.database.DatabaseHelper
 
 
+@HiltAndroidApp
 class AndroidApplication : Application() {
     override fun onCreate() {
         super.onCreate()
