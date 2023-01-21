9ad77dd52e347783febba756b81c8747489f082b
behzoddev <behzoddev@gmail.com>
2023-01-21 16:38:32 +0900

    Add SignInButtonUiComponent.kt

---
+++ app/src/main/java/io/github/qobiljon/stressapp/ui/components/SignInButtonUiComponent.kt
@@ -0,0 +1,15 @@
+package io.github.qobiljon.stressapp.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SignInButtonUiComponent() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInButtonUiComponentPreview() {
+
+}
