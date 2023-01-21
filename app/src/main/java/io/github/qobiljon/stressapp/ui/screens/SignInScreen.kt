509a01eac43fe21fa87fb4fd7939578262f12769
behzoddev <behzoddev@gmail.com>
2023-01-21 17:28:07 +0900

    Update SignInScreen

--- app/src/main/java/io/github/qobiljon/stressapp/ui/screens/SignInScreen.kt
+++ app/src/main/java/io/github/qobiljon/stressapp/ui/screens/SignInScreen.kt
@@ -1,13 +1,33 @@
 package io.github.qobiljon.stressapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.qobiljon.stressapp.ui.components.EmailTextFieldUiComponent
+import io.github.qobiljon.stressapp.ui.components.PasswordTextFieldUiComponent
+import io.github.qobiljon.stressapp.ui.components.SignInButtonUiComponent
 
 @Composable
-fun SignInScreen() {
-
+fun SignInScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        EmailTextFieldUiComponent()
+        PasswordTextFieldUiComponent()
+        SignInButtonUiComponent(onClick = {})
+    }
 }
 
 @Composable
+@Preview(showBackground = true)
 fun SignInScreenPreview() {
-
+    SignInScreen()
 }
